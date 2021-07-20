691e6fcc4ad39312f346015d9f93f61884bdadb6
Robert Chukwuemeka <nekerobert@gmail.com>
2021-07-21 00:32:15 +0100

    modified index page on public folder

--- public/staff/index.php
+++ public/staff/index.php
@@ -5,7 +5,7 @@
 
 <div id="content">
   <div id="main-menu">
-    <h2>Main Menu</h2>
+    <h2>The Main Menu</h2>
     <ul>
       <li><a href="<?php echo url_for('/staff/subjects/index.php'); ?>">Subjects</a></li>
       <li><a href="<?php echo url_for('/staff/pages/index.php'); ?>">Pages</a></li>
